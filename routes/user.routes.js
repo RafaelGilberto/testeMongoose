5308dfd99b77c7c3e1965cf72f9f36e462cffcda
Rafael Gilberto Rezende Lima <rgrl516@gmail.com>
2023-02-23 21:15:39 -0300

    put,create,delete,get

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -17,4 +17,54 @@ userRouter.post("/create-user", async (req, res) => {
   }
 });
 
+userRouter.get("/allUsers", async (req, res) => {
+  try {
+    const users = await UserModel.find();
+    return res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: "Algo deu errado" });
+  }
+});
+
+userRouter.get("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const singleUser = await UserModel.findById(id);
+    if (!singleUser) {
+      return res.status(404).json("Não encontrado");
+    }
+    return res.status(200).json(singleUser);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: "Algo deu errado" });
+  }
+});
+
+userRouter.put("/edit/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedAlbum = await AlbumModel.findOneAndUpdate(
+      { _id: id },
+      { ...req.body },
+      { new: true, runValidators: true }
+    );
+    return res.status(200).json(updatedAlbum);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: "Algo deu errado" });
+  }
+});
+
+userRouter.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteUser = await AlbumModel.findByIdAndDelete(id);
+    return res.status(200).json(deleteUser);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: "Algo deu errado" });
+  }
+});
+
 export default userRouter;
